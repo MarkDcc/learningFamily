6bcf9b7abb7cddef13f940626637cb34ec6390d7
Cheng-Cheng Dong <cheng-cheng.dong@microfocus.com>
2022-09-01 16:41:01 +0800

    add project

---
+++ springboot/src/test/java/com/gordon/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gordon.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
